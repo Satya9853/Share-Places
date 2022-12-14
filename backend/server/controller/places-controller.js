3b5365cc8ab475b10cbb770f6f05e1021d766d75
satya bhusan prusty <satyabhusan9853@gmail.com>
2022-12-14 20:55:40 +0530

    Created the Place Controller with added Custom Error Handlers

--- backend/server/controller/places-controller.js
+++ backend/server/controller/places-controller.js
@@ -1,36 +1,70 @@
 const { StatusCodes } = require("http-status-codes");
-const { BadRequestError, NotFoundError } = require("../errors/index");
+const { validationResult } = require("express-validator");
 
+const { BadRequestError, NotFoundError, CustomApiError } = require("../errors/index");
 const getCoordsForAddress = require("../util/location");
 const PlaceModel = require("../model/places-schema");
 
 exports.getPlaceByID = async (req, res, next) => {
   const placeID = req.params.placeID;
+
   const place = await PlaceModel.findById(placeID);
+
   if (!place) throw new NotFoundError(`No place was found with placeID ${placeID}`);
+
   res.status(StatusCodes.OK).json({ place: place.toObject({ getters: true }) });
 };
 
 exports.getPlacesByUserID = async (req, res, next) => {
   const userID = req.params.userID;
+
   const places = await PlaceModel.find({ creator: userID });
+
+  if (!places) throw new NotFoundError(`No place was found with userID ${userID}`);
+
   places = places.map((place) => place.toObject({ getters: true }));
-  res.status(200).json({ places: places });
+  res.status(StatusCodes.OK).json({ places: places });
 };
 
 exports.createPlace = async (req, res, next) => {
-  const place = await new PlaceModel.create({ ...req.body });
-  await place.save();
-  res.status(200).json({ ...place });
+  const error = validationResult(req);
+  if (!error.isEmpty()) throw new BadRequestError("Invalid inputs passed, please check your data");
+
+  const { address } = req.body;
+
+  const coordinates = getCoordsForAddress(address);
+  if (!coordinates) throw new Error();
+
+  const newPlace = {
+    ...req.body,
+    location: coordinates,
+  };
+
+  const createdPlace = await PlaceModel.create(newPlace);
+
+  if (!createdPlace) throw new Error();
+
+  res.status(StatusCodes.CREATED).json({ place: createdPlace.toObject({ getters: true }) });
 };
 
-exports.updatePlaceByID = (req, res, next) => {
+exports.updatePlaceByID = async (req, res, next) => {
+  const error = validationResult(req);
+  if (!error.isEmpty()) throw new BadRequestError("Invalid inputs passed, please check your data");
+
   const placeID = req.params;
-  const data = req.body;
-  // const updatedPlace = await PlaceModel.fin
-  res.status(200).json({ message: "Working" });
+  const updatedPlace = await PlaceModel.findByIdAndUpdate({ _id: placeID }, req.body, { new: true, runValidators: true });
+
+  if (!updatedPlace) throw new NotFoundError(`No place was found with placeID :${placeID}`);
+
+  res.status(StatusCodes.OK).json({ place: updatedPlace.toObject({ getters: true }) });
 };
 
-exports.deletePlaceByID = (req, res, next) => {
-  res.status(200).json({ message: "Working" });
+exports.deletePlaceByID = async (req, res, next) => {
+  const placeID = req.params.placeID;
+
+  const deletedPlace = await PlaceModel.findOneAndRemove({ _id: placeID });
+
+  if (!deletedPlace) throw new NotFoundError(`No place was found with placeID :${placeID}`);
+
+  res.status(StatusCodes.OK).json({ place: deletedPlace.toObject({ getters: true }) });
 };
--- backend/server/controller/users-controller.js
+++ backend/server/controller/users-controller.js
@@ -1,11 +1,19 @@
-const userModel = require("../model/users-schema");
+const { StatusCodes } = require("http-status-codes");
+
+const UserModel = require("../model/users-schema");
+const { BadRequestError, NotFoundError, CustomApiError } = require("../errors/index");
 
 exports.getUsers = async (req, res, next) => {
-  res.status(200).json({ message: "Working" });
+  const users = await UserModel.find({}).select("-password");
+
+  if (!users) throw new NotFoundError("Currently there are no users");
+
+  users = users.map((user) => user.toObject({ getters: true }));
+
+  res.status(StatusCodes.OK).json({ users });
 };
-exports.userSignup = async (req, res, next) => {
-  res.status(200).json({ message: "Working" });
-};
+
+exports.userSignup = async (req, res, next) => {};
 exports.userLogin = async (req, res, next) => {
   res.status(200).json({ message: "Working" });
 };
